946a91d674ec94b8b90c1270edb1f39b93eb0647
fabian <fabian.huch@gmail.com>
2018-04-29 18:57:12 +0200

    Added user input

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,31 @@
+use std::io;
+
 mod parser;
 mod prover;
 
 fn main() {
-    let formula = "-F, G & F =>";
-    let expr = parser::parse_expression(&formula);
-    println!("{:?}", &formula);
+    loop {
+        println!("Enter formula (in form \"-A | B & C > D => 1\") or q to quit:");
 
-    let mut root = prover::ProofNode::new(Box::from(expr));
+        let mut expression = String::new();
+        io::stdin().read_line(&mut expression).expect("Failed to read line");
+        if expression.ends_with("\n") {
+            expression.pop();
+        }
 
-    if root.proof() {
-        println!("Statement is valid. Proof tree: {:?}", root);
-    } else {
-        println!("Not provable. Partial proof tree: {:?}", root);
+        if expression == "q" {
+            break;
+        }
+
+        let expr = parser::parse_expression(expression.as_str());
+        println!("Parsed expression: {:?}", &expr);
+
+        let mut root = prover::ProofNode::new(Box::from(expr));
+
+        if root.proof() {
+            println!("Statement is valid. Proof tree: {:?}", root);
+        } else {
+            println!("Not provable. Partial proof tree: {:?}", root);
+        }
     }
 }
